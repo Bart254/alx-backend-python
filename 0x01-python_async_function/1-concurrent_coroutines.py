5e062283e73859eab4ba06dc7cda9723c161514f
Bart254 <omondibartlett@gmail.com>
2024-06-10 20:11:44 +0300

    add file 1-concurrent_coroutines.py

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Concurrent coroutines module
+"""
+import time
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """ wait_n function executes coroutine n number of times
+
+    Args:
+        n (int): number of times to execute coroutine
+
+    Returns:
+        list[float]: delay times per coroutine execution
+    """
+    # create tasks
+    tasks = [wait_random() for _ in range(n)]
+
+    # await and capture tasks
+    results = []
+    for task in asyncio.as_completed(tasks):
+        results.append(await task)
+
+    return results
